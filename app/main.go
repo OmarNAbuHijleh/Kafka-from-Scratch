5d5d57032ea47be53746b4f8fde90cf71340d321
Omar Abu-Hijleh <omara569@gmail.com>
2025-05-02 04:14:39 -0500

    [any message]

--- app/main.go
+++ app/main.go
@@ -34,7 +34,7 @@ func main() {
 // This function takes in a net.Conn struct and obtains the header value from it. Following this, it will return a response
 func handleConnection(conn net.Conn) {
 	// 3 parts to our response
-	// var message_size int32
+	var message_size int32
 	var correlation_id int32 // This is part of the response header
 	var request_api_version int16
 	var request_api_key int16
@@ -59,15 +59,14 @@ func handleConnection(conn net.Conn) {
 	}
 
 	// message_size is first 4 bytes
-	// message_size = int32(binary.BigEndian.Uint32(buffer[0:4])) // A number value representing the number of bits/bytes including the 32 bits of the message_size portion!
+	message_size = int32(binary.BigEndian.Uint32(buffer[0:4])) // A number value representing the number of bits/bytes including the 32 bits of the message_size portion!
 	// Now we need to parse so that of the 12 bytes we have, we take the 8th through 11th bytes
 	correlation_id = int32(binary.BigEndian.Uint32(buffer[8:12]))     // 4 bytes
 	request_api_version = int16(binary.BigEndian.Uint16(buffer[6:8])) // 2 bytes
 	request_api_key = int16(binary.BigEndian.Uint16(buffer[4:6]))     // 2 bytes
 
 	//Now we want to send the binary values
-	// binary.Write(conn, binary.BigEndian, message_size)   // 4 bytes in length. The value we write may be subject to change
-	binary.Write(conn, binary.BigEndian, int32(20))      // 4 bytes in length. The value we write may be subject to change
+	binary.Write(conn, binary.BigEndian, message_size)   // 4 bytes in length. The value we write may be subject to change
 	binary.Write(conn, binary.BigEndian, correlation_id) // 4 bytes
 	api_error_code := valid_version(request_api_version)
 	binary.Write(conn, binary.BigEndian, api_error_code) // 2 bytes
